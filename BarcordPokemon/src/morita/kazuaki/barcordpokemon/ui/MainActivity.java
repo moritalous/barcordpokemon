16122aa6166404d187e894da8381c1273dfecce2
moritalous <moritalous@gmail.com>
2014-08-13 01:40:27 +0900

    レイアウトを調整

--- BarcordPokemon/src/morita/kazuaki/barcordpokemon/ui/MainActivity.java
+++ BarcordPokemon/src/morita/kazuaki/barcordpokemon/ui/MainActivity.java
@@ -4,8 +4,6 @@ import morita.kazuaki.barcordpokemon.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -26,25 +24,6 @@ public class MainActivity extends Activity implements OnClickListener {
 		gotten = (Button) findViewById(R.id.btn_gotten);
 		gotten.setOnClickListener(this);
 
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
